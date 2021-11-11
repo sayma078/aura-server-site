1a5cdd55121b2dd74303195d0146fed9e168d0ca
sayma078 <somayasayma2@gmail.com>
2021-11-11 22:02:40 +0600

    3rd add

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const app = express();
+
 const { MongoClient } = require('mongodb');
+
+const ObjectId = require("mongodb").ObjectId;
+
 
 const cors = require("cors");
 require("dotenv").config();
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -14,6 +18,44 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.31irp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // console.log(uri)
+
+async function run (){
+    try{
+        await client.connect();
+        // console.log('connect to database', );
+        const database = client.db("Aura")
+        const productsCollection = database.collection("products")
+
+
+        //get API
+        app.get('/products', async(req, res) =>{
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+      res.send(products);
+        })
+        //post API
+      app.post('/products', async(req, res) =>{
+          const product = req.body;
+          console.log('hit the post api ', product)
+
+          const result = await productsCollection.insertOne(product);
+          console.log(result);
+          res.json(result);
+    });
+    //GET Single product
+    app.get("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log("getting specifing product", id);
+        const query = { _id: ObjectId(id) };
+        const products = await productsCollection.findOne(query);
+        res.json(products);
+      });
+    }
+    finally{
+        //await client.close()
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Running Aura Server");
